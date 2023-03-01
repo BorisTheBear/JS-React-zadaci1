df5e622491aec5c7f396118d17ee66d627036ac9
BorisTheBear <boris.j911@gmail.com>
2023-03-01 20:57:18 +0100

    Zadatak 3

--- src/AnimalList.js
+++ src/AnimalList.js
@@ -1,16 +1,14 @@
 import React from "react";
 import SingleAnimal from "./SingleAnimal";
 
-export default function AnimalList({ animals }) {
-  // let animalNumber = 0;
-
+export default function AnimalList({ animals, removeAnimal }) {
   return animals.map((element, index) => {
-    // animalNumber++;
     return (
       <div key={index}>
-        <h4>Animal no.{index}</h4>
+        <h4>Animal no.{index + 1}</h4>
         <ul>
           <SingleAnimal animal={element} />
+          <button onClick={() => removeAnimal(element)}>Remove</button>
         </ul>
       </div>
     );
--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react';
-import AnimalList from './AnimalList.js'
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import AnimalList from "./AnimalList.js";
 
 function App() {
   const [animals, setAnimals] = useState([
-    {species: "dog", name: "dog name", dateOfBirth: null},
-    {species: "cat", name: "cat name", dateOfBirth: new Date()},
-    {species: "horse", name: "horse name", dateOfBirth: new Date()},
-    {species: "cow", name: "cow name", dateOfBirth: new Date()},
-    {species: "elephant", name: "elephant name", dateOfBirth: new Date()},
+    { species: "dog", name: "dog name", dateOfBirth: null },
+    { species: "cat", name: "cat name", dateOfBirth: new Date() },
+    { species: "horse", name: "horse name", dateOfBirth: new Date() },
+    { species: "cow", name: "cow name", dateOfBirth: new Date() },
+    { species: "elephant", name: "elephant name", dateOfBirth: new Date() },
   ]);
-  
+  const removeAnimal = (animal) => {
+    setAnimals([
+      ...animals.slice(0, animals.indexOf(animal)),
+      ...animals.slice(animals.indexOf(animal) + 1, animals.length),
+    ]);
+  };
 
   return (
     <div>
       <h4>List of animals in the zoo:</h4>
-      <AnimalList animals={animals} />
+      <AnimalList animals={animals} removeAnimal={removeAnimal} />
     </div>
   );
 }
